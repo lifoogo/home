07539da0c234ef8b705686ed57ac4ee8223912d6
lifoogo <aleksandr111111111111111111111@gmail.com>
2019-11-29 22:29:52 +0300

    header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css'
+import Header from './components/header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
